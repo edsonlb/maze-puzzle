f9048c69f9e6df304f7aa9977a5d1b98b9d5b145
edsonlb <ti_edson@yahoo.com.br>
2017-04-17 22:51:56 +0200

    Updating Maze files to make the table

--- Maze.py
+++ Maze.py
@@ -9,17 +9,21 @@ class Maze:
         self.search_parameters = search_parameters
         self.output = [['ID', 'Room', 'Object collected']]
         self.json_array = self.validate_json(json_file)
-        #self.get_item_from_id(int(start_node))
+        self.search_elements(self.get_item_from_id(int(start_node)))
 
     def get_item_from_id(self, my_id):
         """Return internal item from a posision based on the item ID field."""
-        array_id = (obj for obj in self.json_array['rooms'] if obj["id"] == my_id).next()
+        try:
+            obj_id = (obj for obj in self.json_array['rooms'] if obj["id"] == my_id).next()
 
-        if array_id:
-            return array_id
-        else:
-            print 'END OF ITEMS.'
-            sys.exit(0)
+            if obj_id:
+                return obj_id
+            else:
+                print 'YOU DONT KNOW HOW TO PLAY :-)'
+                return None
+        except:
+            print 'YOU DONT KNOW HOW TO PLAY :-)'
+            return None
 
     def validate_json(self, my_file):
         """Because the "object" node made by "Scm Italy" was not in the JSON standards."""
@@ -35,7 +39,55 @@ class Maze:
                 print 'VALIDATING JSON WITH OBJECT TAG CORRECTION...'
                 return json.loads(my_file.read().replace('objects', '"objects"'))
             except:
-                print 'YOU DONT KNOW HOW TO PLAY :-)'
+                print 'THE FILE IS NOT A JSON!'
+
+
+    def search_elements(self, my_item):
+        """Get the elements on the "object" node of an item list."""
+        if not my_item:
+            return None
+
+        needles_found = []
+
+        for needle in self.search_parameters:
+            try:
+                obj_found = (obj for obj in my_item['objects'] if obj["name"].upper() == needle.upper()).next()
+                needles_found.append(obj_found['name'])
+            except:
+                pass
+
+        if needles_found:
+            objects_collected = ','.join(needles_found)
+        else:
+            objects_collected = 'None'
+
+        self.output.append([my_item['id'], my_item['name'], objects_collected])
+        self.navigate(my_item)
+
+
+    def navigate(self, my_item):
+        """Get the direction of the itens navigation."""
+        position_of_item = [i for i, j in enumerate(self.json_array['rooms']) if j == my_item]
+
+        if 'north' in my_item:
+            new_node = my_item['north']
+            self.json_array['rooms'][position_of_item[0]].pop('north')
+            self.search_elements(self.get_item_from_id(new_node))
+
+        if 'south' in my_item:
+            new_node = my_item['south']
+            self.json_array['rooms'][position_of_item[0]].pop('south')
+            self.search_elements(self.get_item_from_id(new_node))
+
+        if 'west' in my_item:
+            new_node = my_item['west']
+            self.json_array['rooms'][position_of_item[0]].pop('west')
+            self.search_elements(self.get_item_from_id(new_node))
+
+        if 'east' in my_item:
+            new_node = my_item['east']
+            self.json_array['rooms'][position_of_item[0]].pop('east')
+            self.search_elements(self.get_item_from_id(new_node))
 
     def print_output(self):
         """Lets print this"""
